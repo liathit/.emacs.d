632fa47ba3cb75068cd4668e3df9bb2ea8f76d19
Serge Suvorov <sssuvor66@gmail.com>
2015-10-26 20:13:51 +0300

    first commit

---
+++ init.el
@@ -0,0 +1,24 @@
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
+(setq inhibit-startup-message t)
+
+;;; Set up package
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish) 
+(require 'bind-key)
+
+(org-babel-load-file (concat user-emacs-directory "config.org"))
