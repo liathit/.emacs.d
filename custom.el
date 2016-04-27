08f72b785656827fa042d4eda4e9a11c3b917de4
Serge Suvorov <sssuvor66@gmail.com>
2016-04-28 01:54:06 +0300

    Emoji

--- custom.el
+++ custom.el
@@ -5,10 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("0b6645497e51d80eda1d337d6cabe31814d6c381e69491931a688836c16137ed" "cdfb22711f64d0e665f40b2607879fcf2607764b2b70d672ddaa26d2da13049f" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("45712b65018922c9173439d9b1b193cb406f725f14d02c8c33e0d2cdad844613" "0b6645497e51d80eda1d337d6cabe31814d6c381e69491931a688836c16137ed" "cdfb22711f64d0e665f40b2607879fcf2607764b2b70d672ddaa26d2da13049f" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(elfeed-feeds
    (quote
-    ("https://www.linux.org.ru/section-rss.jsp?section=1"))))
+    ("https://www.linux.org.ru/section-rss.jsp?section=1")))
+ '(package-selected-packages
+   (quote
+    (magit emoji-cheat-sheet-plus ac-emoji emojify which-key use-package swiper powerline perspective paredit org-alert jedi jbeans-theme helm-swoop helm-projectile ac-cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
